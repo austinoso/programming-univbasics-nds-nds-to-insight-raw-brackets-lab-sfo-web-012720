62bc0773919a74f4d442ec5346b5e82c8944d4aa
Austin Osorio <osorio_austin@yahoo.com>
2020-01-11 07:07:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,11 +5,11 @@ def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
 
-  row_index = 0
+  director_index = 0
 
   while row_index < nds.length do
     column_index = 0
-
+    while column_index < nds[row_index]
   end
 
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
